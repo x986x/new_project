85ab97442e5f522af404e0be71fb5082cedb8484
Dmitry Lepyanske <lepyanske@yandex.ru>
2023-09-27 12:00:58 +0300

    добавил функцию, которая принимает на вход строку и возвращает ее словами начинающимся с заглавной буквы

--- str_func.py
+++ str_func.py
@@ -4,3 +4,15 @@ def capital_letters(input_string):
 input_string = input()
 result = capital_letters(input_string)
 print(result)
+
+def capitalize_words(input_string):
+    words = input_string.split()
+    capitalize_words = [word.capitalize() for word in words]
+    return ' '.join(capitalize_words)
+
+def main():
+    result = capitalize_words(input_string)
+    print(result)
+
+if __name__ == "__main__":
+    main()
